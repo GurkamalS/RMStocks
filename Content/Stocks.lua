de3640ddbfb5ef8312c59aa8debb6777039f64a7
Sinbad <GurkamalS@users.noreply.github.com>
2019-10-19 20:51:51 -0400

    Lua fileupdates
    Updated lua file - commented parts that are easier to understand and marked issues. Need to update the IEX pull from google finance, and add an auth token.

--- Content/Stocks.lua
+++ Content/Stocks.lua
@@ -1,100 +1,94 @@
 function Initialize()
-	VariantWidth = SKIN:GetVariable('Variant', 'Normal')
-	if string.find (VariantWidth, 'Wide', 1, plain) then
-		print('Width: Wide variant\n')
-	elseif string.find (VariantWidth, 'Normal', 1, plain) then
-		print('Width: Normal variant\n')
+	VariantWidth = SKIN:GetVariable('Variant', 'Ticker')-- can be Ticker, Stocks, or Wide
 	end
-	
+
 	numStocks = 0
 	numUpdates = 0
 	local i = 0
 	Stocks = ReadStocks()
 --	numStocks = 8 -- for testing
-	
+
 	while i < numStocks do
 		i = i + 1
 		SKIN:Bang('!SetVariable', 'Stock'..i, Stocks[i])
 --		print('Assigning variable Stock'..i..' = '..Stocks[i])
 	end
-	SKIN:Bang('!SetVariable', 'NumberOfStocks', numStocks)	
+	SKIN:Bang('!SetVariable', 'NumberOfStocks', numStocks)
 
-	WriteMeasures()	
-	WriteQuotes()
-	
+	WriteMeasures()
+	WriteQuotes()--improve this
+
 end -- function Initialize
 
 function Update()
---	numUpdates = numUpdates + 1
+numUpdates = numUpdates + 1
 	SKIN:Bang('!SetVariable', 'NumUpdates', numUpdates)
 end -- function Update
 
 function ReadStocks()
-	local FileP = SKIN:MakePathAbsolute('@Resources\\Stocks.txt')
+	local FileP = SKIN:MakePathAbsolute('@Resources\\Stocks.txt')--oops, I had moved it.
 --	print('Reading Stocks file at ' ..FileP )
-	
+
 	local File = io.open(FileP, 'r')
 	if not File then
 		print('ReadFile: unable to open file at ' ..FileP )
 		return
 	end
-	
-	
-	local contents = {}	
+
+
+	local contents = {}
 	for Line in File:lines() do
 		if Line ~= '' then
 			table.insert(contents, Line)
 			numStocks = numStocks + 1
 		end
 	end
-	
+
 	File:close()
-	print('ReadStocks(): Read in '..numStocks..' stocks from file '..FileP..'.')
+	print('ReadStocks(): Read in '..numStocks..' stocks from file '..FilePath..'.')
 	return(contents)
 end
 
 function WriteMeasures()
-	
+
 	local i = 0
-	local FilePath = SKIN:MakePathAbsolute('@Resources\\Measures.inc')
-	
+	local FilePath = SKIN:MakePathAbsolute('@Resources\\Measures.inc')--Don't move that either
+
 	-- OPEN FILE
 	print('WriteMeasures: Opening file for writing at '..FilePath)
 	local f = io.open(FilePath, 'w')
-	
+
 	-- ERROR CHECKING
 	if not f then
-		print('WriteMeasures: Unable to open file at '..FilePath)
+		error print('WriteMeasures: Unable to open file at '..FilePath)
 		return
 	end
 
 
-	f:write('; Automatically Generated File - please do not edit\n')
+	f:write('; Automatically Generated File - please do not edit. Refer to the lua\n')
 	while i < numStocks do
 		i = i + 1
 		f:write('\n')
 		f:write('[MeasureStock'..i..']\n')
 		f:write('Measure=Plugin\n')
-		f:write('Plugin=Plugins\\WebParser.dll\n')
-		f:write('!Delay 5000\n')				
+		f:write('Plugin=Plugins\\WebParser.dll\n')-- see note below
+		f:write('!Delay 5000\n')
 --		f:write('Debug=2\n')
-		f:write('URL=https://www.google.com/finance?q=#stock'..i..'#\n')
-		f:write('RegExp=(?siU).*<h3 .*</a><span> - (.*)</span></h3></div>.*<tr><td colspan="3"><span style="font-size:.*%"><b>(.*)</b>&nbsp;<cite style="color:.*"><b>(.*)</b> \\((.*)%\\)</cite></span><br><span class="f">(.*)</span>.*\n')
-		
+		f:write('URL='..i..'#\n')-- google api prvents standard calls. Remove that, switch to iex
 --		f:write('RegExp=(?siU).*<meta itemprop="name".*content="(.*)".*<meta itemprop="url".*content="(.*)".*<meta itemprop="price".*content="(.*)".*<meta itemprop="priceChange".*content="(.*)".*<meta itemprop="priceChangePercent".*content="(.*)".*\n')
---		
-		f:write('UpdateRate=6000\n')
+--
+		f:write('UpdateRate=100\n')--iex api allows far faster updates
 		f:write('DynamicVariables=1\n')
--- Stock 'long' name		
+-- Stock 'long' name
 		f:write('[MeasureStockName'..i..']\n')
 		f:write('Measure=Plugin\n')
-		f:write('Plugin=Plugins\\WebParser.dll\n')
+		f:write('Plugin=Plugins\\WebParser.dll\n')-- is this even necessary? New rainmeter standards preclude this
 		f:write('URL=[MeasureStock'..i..']\n')
 		f:write('StringIndex=1\n')
 -- Stock price
 		f:write('[MeasureStockPrice'..i..']\n')
 		f:write('Measure=Plugin\n')
-		f:write('Plugin=Plugins\\WebParser.dll\n')
+		f:write('Plugin=Plugins\\WebParser.dll\n')--see note above
 		f:write('URL=[MeasureStock'..i..']\n')
 		f:write('StringIndex=2\n')
 -- Stock price change
@@ -107,7 +101,7 @@ function WriteMeasures()
 		f:write('[MeasureStockPriceChangePerCent'..i..']\n')
 		f:write('Measure=Plugin\n')
 		f:write('Plugin=Plugins\\WebParser.dll\n')
-		f:write('URL=[MeasureStock'..i..']\n')
+		f:write('URL=[MeasureStock'..i..']\n')--IEX doesn't offer intraday, and how should this respond when the market is closed?
 		f:write('StringIndex=4\n')
 -- Stock last update time
 		f:write('[MeasureStockUpdateTime'..i..']\n')
@@ -120,25 +114,21 @@ function WriteMeasures()
 end
 
 function WriteQuotes()
-	
+
 	local i = 0
-	local currency = 0
+	local currency = 0-- 0 is USD
 	local FilePath
-	
+
 	VariantWidth = SKIN:GetVariable('Variant', 'Normal')
-	if string.find (VariantWidth, 'Wide', 1, plain) then
-		FilePath = SKIN:MakePathAbsolute('@Resources\\Quotes-wide.inc')			
-	else
-		FilePath = SKIN:MakePathAbsolute('@Resources\\Quotes-normal.inc')
 	end
 
 
---	local FilePath = SKIN:MakePathAbsolute('@Resources\\Quotes.inc')
-	
+local FilePath = SKIN:MakePathAbsolute('@Resources\\Quotes.inc')
+
 	-- OPEN FILE
 	print('WriteQuotes Opening file for writing at '..FilePath)
 	local f = io.open(FilePath, 'w')
-	
+
 	-- ERROR CHECKING
 	if not f then
 		print('WriteQuotes: Unable to open file at '..FilePath)
@@ -154,15 +144,15 @@ function WriteQuotes()
 		f:write('MeterStyle=sTextLeft\n')
 		f:write('ClipString=2\n')
 		f:write('W=#Col1Width#\n')
-		f:write('H=#Col1Height#\n')						
+		f:write('H=#Col1Height#\n')
 		f:write('X=#Col1XPos#\n')
 		f:write('Y=0R\n')
-		
+
 		currency = 0
 		currency = string.find(Stocks[i], 'CURRENCY:', 1, plain)
 		if currency == 1 then
 			f:write('Text='..string.sub(Stocks[i],10, 12)..' [\\x00BB] '..string.sub(Stocks[i],-3)..'\n')
-			
+
 		else
 			if string.find (VariantWidth, 'Wide', 1, plain) then
 				f:write('Text=[MeasureStockName'..i..']\n')
@@ -216,6 +206,3 @@ function WriteQuotes()
 	f:close()
 
 end
-
-
-
